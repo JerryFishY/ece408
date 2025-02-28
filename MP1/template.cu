f65c350d3c5d21ee4973b047eada645646ddeee4
Ray Yu <1446321377y@gmail.com>
2025-02-28 15:13:55 +0800

    Finish MP1

--- MP1/template.cu
+++ MP1/template.cu
@@ -3,6 +3,9 @@
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   //@@ Insert code to implement vector addition here
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if(idx<len)
+    out[idx]=in1[idx]+in2[idx];
 }
 
 int main(int argc, char **argv) {
@@ -28,31 +31,42 @@ int main(int argc, char **argv) {
   wbLog(TRACE, "The input length is ", inputLength);
 
   wbTime_start(GPU, "Allocating GPU memory.");
+  size_t size = inputLength * sizeof(float);
   //@@ Allocate GPU memory here
+  cudaMalloc((void **) &deviceInput1, size);
+  cudaMalloc((void **) &deviceInput2, size);
+  cudaMalloc((void **) &deviceOutput, size);
 
   wbTime_stop(GPU, "Allocating GPU memory.");
 
   wbTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
+  cudaMemcpy(deviceInput1, hostInput1, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInput2, hostInput2, size, cudaMemcpyHostToDevice);
 
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
-
+  dim3 DimGrid(ceil(inputLength/256.0), 1, 1);
+  dim3 DimBlock(256, 1, 1);
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
+  vecAdd<<<DimGrid, DimBlock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
 
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Performing CUDA computation");
 
   wbTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
-
+  cudaMemcpy(hostOutput, deviceOutput, size, cudaMemcpyDeviceToHost);
   wbTime_stop(Copy, "Copying output memory to the CPU");
 
   wbTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
-
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
+  
   wbTime_stop(GPU, "Freeing GPU Memory");
 
   wbSolution(args, hostOutput, inputLength);
