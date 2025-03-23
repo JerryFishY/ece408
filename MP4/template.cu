7a3bca85925a4f86f50401638a60a52595bb12ca
Ray Yu <1446321377y@gmail.com>
2025-03-23 23:48:29 +0800

    Finish MP4

--- MP4/template.cu
+++ MP4/template.cu
@@ -1,3 +1,4 @@
+// Input parallel is used for this MP.
 #include <wb.h>
 
 #define wbCheck(stmt)                                                     \
@@ -11,12 +12,44 @@
   } while (0)
 
 //@@ Define any useful program-wide constants here
-
+constexpr int PADED_TILE_SIZE = 10; // 1+8+1 with padding around the block
+constexpr int TILE_SIZE = 8; // 8 is the tile size
 //@@ Define constant memory for device kernel here
+__constant__ float constant_kernel[27];
 
 __global__ void conv3d(float *input, float *output, const int z_size,
                        const int y_size, const int x_size) {
   //@@ Insert kernel code here
+  __shared__ float N_ds[PADED_TILE_SIZE][PADED_TILE_SIZE][PADED_TILE_SIZE];
+  float Pvalue = 0.0;
+  // Perform loading
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int tz = threadIdx.z;
+  int gx = blockIdx.x * TILE_SIZE + tx - 1;
+  int gy = blockIdx.y * TILE_SIZE + ty - 1;
+  int gz = blockIdx.z * TILE_SIZE + tz - 1;
+  // Load the data
+  if(gx<0 || gy<0 || gz<0 || gx>=x_size || gy>=y_size || gz>=z_size){
+    N_ds[tz][ty][tx] = 0.0;
+  }else{
+    N_ds[tz][ty][tx] = input[gz * y_size * x_size + gy * x_size + gx];
+  }
+  __syncthreads();
+
+  //Computational part
+  if(tx<TILE_SIZE && ty<TILE_SIZE && tz<TILE_SIZE && gx+1<x_size && gy+1<y_size && gz+1<z_size){
+    // Perform the convolution
+    #pragma unroll
+    for (int i = 0;i<3;i++){
+      for (int j = 0;j<3;j++){
+        for (int k = 0;k<3;k++){
+          Pvalue += N_ds[tz+i][ty+j][tx+k] * constant_kernel[i*3*3+j*3+k];
+        }
+      }
+    }
+    output[(gz+1) * y_size * x_size + (gy+1) * x_size + gx+1] = Pvalue;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -52,7 +85,9 @@ int main(int argc, char *argv[]) {
   wbTime_start(GPU, "Doing GPU memory allocation");
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
-  // because the first  three elements were the dimensions
+  // because the first three elements were the dimensions
+  cudaMalloc((void **)&deviceInput, (inputLength - 3) * sizeof(float));
+  cudaMalloc((void **)&deviceOutput, (inputLength - 3) * sizeof(float));
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
@@ -60,17 +95,23 @@ int main(int argc, char *argv[]) {
   // Recall that the first three elements of hostInput are dimensions and
   // do
   // not need to be copied to the gpu
+  cudaMemcpy(deviceInput,hostInput+3,(inputLength - 3) * sizeof(float),cudaMemcpyHostToDevice);
+  cudaMemcpyToSymbol(constant_kernel,hostKernel,27*sizeof(float));
   wbTime_stop(Copy, "Copying data to the GPU");
 
   wbTime_start(Compute, "Doing the computation on the GPU");
   //@@ Initialize grid and block dimensions here
-
+  dim3 DimGrid(ceil(x_size/8.0),ceil(y_size/8.0),ceil(z_size/8.0));
+  dim3 DimBlock(PADED_TILE_SIZE,PADED_TILE_SIZE,PADED_TILE_SIZE);
   //@@ Launch the GPU kernel here
+  conv3d<<<DimGrid,DimBlock>>>(deviceInput,deviceOutput,z_size,y_size,x_size);
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   wbTime_start(Copy, "Copying data from the GPU");
   //@@ Copy the device memory back to the host here
+  cudaMemcpy(hostOutput+3,deviceOutput,(inputLength-3) * sizeof(float),cudaMemcpyDeviceToHost);
+
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
   wbTime_stop(Copy, "Copying data from the GPU");
@@ -81,6 +122,7 @@ int main(int argc, char *argv[]) {
   hostOutput[0] = z_size;
   hostOutput[1] = y_size;
   hostOutput[2] = x_size;
+
   wbSolution(args, hostOutput, inputLength);
 
   // Free device memory
